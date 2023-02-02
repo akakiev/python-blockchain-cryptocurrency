152ac9715953204767e4ed9bde01d4a0cd4bd397
Serhii Baraban <akakieva@gmail.com>
2023-02-02 09:19:27 +0100

    accessed the last list item

--- blockchain.py
+++ blockchain.py
@@ -1,10 +1,10 @@
 #Diving into the basics of Python
 
-blockchain = [1]
+blockchain = [[1]]
 
 def add_value():
     #function that appends values
-    blockchain.append([blockchain[0], 5.3])
+    blockchain.append([blockchain[-1], 5.3])
     print(blockchain)
 
 for i in range (5):
