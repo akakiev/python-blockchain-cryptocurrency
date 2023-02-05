2be962db31bb10d611919e207cd239b8a85ac40f
Serhii Baraban <akakieva@gmail.com>
2023-02-06 00:12:47 +0100

    added else statement to the while and for loops

--- blockchain.py
+++ blockchain.py
@@ -37,6 +37,8 @@ def print_blockchain_elements():
     for block in blockchain:
         print('Outputting Block')
         print(block)
+    else:
+        print(20 * '-')
 
 
 def verify_chain():
@@ -54,8 +56,9 @@ def verify_chain():
         block_index += 1
     return is_valid
 
+waiting_for_input = True
 
-while True:
+while waiting_for_input:
     print('Please choose')
     print('1: Add a new transaction value')
     print('2: Output the blockchain blocks')
@@ -71,12 +74,14 @@ while True:
         if len(blockchain) >= 1:
             blockchain[0] = [2]
     elif user_choice == 'q':
-        break
+        waiting_for_input = False
     else:
         print('Input was invalid, please pick a value from the list!')
     if not verify_chain():
         print('Invalid blockchain!')
         break
+else:
+    print('User left!')
     
 
 print('Done')
