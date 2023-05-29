4b20cedb76bc628f6c1fe6aafecf2b32004e9724
Serhii Baraban <akakieva@gmail.com>
2023-05-29 12:13:54 +0200

    used best practices

--- data-structures-assignment-1-problem/assignment.py
+++ data-structures-assignment-1-problem/assignment.py
@@ -6,12 +6,15 @@ list_names = [person['name'] for person in persons_info]
 print(list_names)
 # 3) Use a list comprehension to check whether all persons are older than 20.
 older_persons = [person for person in persons_info if person['age'] > 20]
+are_older = all([person['age'] > 20 for person in persons_info])
+print(are_older)
 if len(older_persons) == len(list_names):
     print(True)
 else:
     print(False)
 # 4) Copy the person list such that you can safely edit the name of the first person (without changing the original list).
-copied_persons = persons_info[:]
+# copied_persons = persons_info[:]
+copied_persons = [person.copy() for person in persons_info]
 copied_persons[0]['name'] = 'Inna'
 print(copied_persons)
 # 5) Unpack the persons of the original list into different variables and output these variables.
