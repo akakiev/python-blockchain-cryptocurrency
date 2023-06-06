bb0b1b478cb0d36b9e7731ef079f5e0b5ba2751d
Serhii Baraban <akakieva@gmail.com>
2023-06-06 13:48:19 +0200

    splitted up the code

---
+++ hash_util.py
@@ -0,0 +1,12 @@
+import json
+import hashlib as hl
+
+def hash_string_256(string):
+    return hl.sha256(string).hexdigest()
+
+def hash_block(block):
+    """Hashes a block and returns a string representation of it.
+    Arguments:
+        :block: The block that should be hashed.
+    """
+    return hash_string_256(json.dumps(block, sort_keys=True).encode())
