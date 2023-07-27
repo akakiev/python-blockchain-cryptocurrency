2b4217dc8ccc74980889972e5c41694efe55af7a
DESKTOP-CMM3H42\Serhii Baraban <akakieva@gmail.com>
2023-07-27 22:16:03 +0200

    added the signature to the transaction

--- transaction.py
+++ transaction.py
@@ -2,10 +2,19 @@ from collections import OrderedDict
 from utility.printable import Printable
 
 class Transaction(Printable):
-    def __init__(self, sender, recipient, amount):
+    """A transaction which can be added to a block in the blockchain.
+
+    Attributes:
+        :sender: The sender of the coins.
+        :recipient: The recipient of the coins.
+        :signature: The signature of the transaction.
+        :amount: The amount of coins sent with the transaction (default = 1.0)
+    """
+    def __init__(self, sender, recipient, signature, amount):
         self.sender = sender
         self.recipient = recipient
         self.amount = amount
+        self.signature = signature
     
     def to_ordered_dict(self):
         return OrderedDict([('sender', self.sender), ('recipient', self.recipient), ('amount', self.amount)])
